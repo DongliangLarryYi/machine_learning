f484cdef230d86da75f4f7eb985861d46e5f4ea3
Dongliang (Larry) Yi <larry.ydl@gmail.com>
2018-06-29 21:45:28 -0500

    Update findClosestCentroids.m

--- machine-learning-ex7/ex7/findClosestCentroids.m
+++ machine-learning-ex7/ex7/findClosestCentroids.m
@@ -21,7 +21,6 @@ idx = zeros(size(X,1), 1);
 % Note: You can use a for-loop over the examples to compute this.
 %
 
-
 m = size(X,1);
 
 for i = 1:m
@@ -34,10 +33,6 @@ error = 10^10;
         idx(i) = j;
     end
 end
-
-
-
-
 
 % =============================================================
 
