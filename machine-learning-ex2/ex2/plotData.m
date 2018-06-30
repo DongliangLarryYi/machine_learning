b9a373430aab662f050d2d7b0ed2d25135bb4c40
Dongliang (Larry) Yi <larry.ydl@gmail.com>
2018-06-29 21:26:36 -0500

    Update plotData.m

--- machine-learning-ex2/ex2/plotData.m
+++ machine-learning-ex2/ex2/plotData.m
@@ -10,9 +10,6 @@ figure; hold on;
 % Instructions: Plot the positive and negative examples on a
 %               2D plot, using the option 'k+' for the positive
 %               examples and 'ko' for the negative examples.
-%
-
-
 
 % Find Indices of Positive and Negative Examples
 pos = find(y==1); neg = find(y == 0);
@@ -22,14 +19,7 @@ plot(X(pos, 1), X(pos, 2), 'k+','LineWidth', 2, ...
 plot(X(neg, 1), X(neg, 2), 'ko', 'MarkerFaceColor', 'y', ...
      'MarkerSize', 7);
 
-
-
-
-
 % =========================================================================
-
-
-
 hold off;
 
 end
