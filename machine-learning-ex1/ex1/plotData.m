d03b7a26d4616fd88cb4680a0dd41973fe1db077
Dongliang (Larry) Yi <larry.ydl@gmail.com>
2018-06-29 21:22:49 -0500

    Update plotData.m

--- machine-learning-ex1/ex1/plotData.m
+++ machine-learning-ex1/ex1/plotData.m
@@ -19,10 +19,5 @@ plot(x, y, 'rx', 'MarkerSize', 10); % Plot the data
 ylabel('Profit in $10,000s'); % Set the y−axis label
 xlabel('Population of City in 10,000s'); % Set the x−axis label
 
-
-
-
-
 % ============================================================
-
 end
