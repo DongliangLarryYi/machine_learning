99e752686a42d7980d5ee261b778fa6694482d4e
Dongliang (Larry) Yi <larry.ydl@gmail.com>
2018-06-29 21:23:14 -0500

    Update computeCost.m

--- machine-learning-ex1/ex1/computeCost.m
+++ machine-learning-ex1/ex1/computeCost.m
@@ -16,10 +16,7 @@ J = 0;
 m = size(X,1);
 predictions = X*theta;
 sqrErrors = (predictions - y).^2;
-
 J = 1/(2*m) * sum(sqrErrors);
-
-
 
 % =========================================================================
 
