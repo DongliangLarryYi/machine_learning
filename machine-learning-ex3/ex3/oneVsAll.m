08694026c539abd355c67922ffab478e33216560
Dongliang (Larry) Yi <larry.ydl@gmail.com>
2018-06-29 21:31:17 -0500

    Update oneVsAll.m

--- machine-learning-ex3/ex3/oneVsAll.m
+++ machine-learning-ex3/ex3/oneVsAll.m
@@ -50,27 +50,14 @@ X = [ones(m, 1) X];
 %
 
 for c = 1:num_labels
-
 initial_theta = zeros(n+1,1);
 options = optimset('GradObj', 'on', 'MaxIter', 50);
      [theta] = ...
          fmincg (@(t)(lrCostFunction(t, X, (y == c), lambda)), ...
                                 initial_theta, options);
 all_theta(c,1:n+1) = theta';
-
-
 endfor
 
-
-
-
-
-
-
-
-
-
 % =========================================================================
-
 
 end
